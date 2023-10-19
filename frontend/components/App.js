bd7e2ee0a85af2c64e0281ea5344bac925fff1a9
Torien3x <tstanley0323@gmail.com>
2023-10-18 23:10:11 -0700

    sprint intro react completion

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import Character from './Character'
 
@@ -7,12 +7,91 @@ const urlPeople = 'http://localhost:9009/api/people'
 
 function App() {
   // ❗ Create state to hold the data from the API
+  const [planetData, setPlanetData] = useState([]);
+  const [peopleData, setPeopleData] = useState([]);
+
   // ❗ Create effects to fetch the data and put it in state
+   useEffect(() => {
+     const fetchData = async () => {
+       try {
+         const [response1, response2] = await Promise.all([
+           axios.get(urlPlanets), // Replace with your first API endpoint
+           axios.get(urlPeople)  // Replace with your second API endpoint
+         ]);
+  
+         setPlanetData(response1.data); // Set data1 state with response from API 1
+         setPeopleData(response2.data); // Set data2 state with response from API 2
+       } catch (error) {
+         // Handle errors here
+         console.error(error);
+       }
+     };
+  
+     fetchData();
+   }, []);
+
+  //  console.log(planetData)
+  //  console.log('peopleData', peopleData)
+
+  
+  let characterInfo = peopleData.map((data) => {
+    let newArr
+    let combinedData = [];
+    planetData.forEach((pData) => {
+      if(data.homeworld === pData.id) {
+        newArr = {...data, homeworld:{...pData}}
+        combinedData.push(newArr)
+      }
+    })
+    return newArr
+   })
+
+   
+  
+
+
+
+//   useEffect(() => {
+//     axios.get(` ${urlPlanets}`)
+//     .then(res => {
+//       setPlanetData(res.data)
+//     })
+//     .catch(err => {
+//       console.log(err)
+//     })
+//   }, [])
+  
+//   useEffect(() => {
+//     axios.get(` ${urlPeople}`)
+//     .then(res => {
+//       setPeopleData(res.data)
+//     })
+//     .catch(err => {
+//       console.log(err)
+//     })
+//   }, [])
+
+// console.log([...planetData, ...peopleData]);
+
+ 
+
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
       {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      {
+        characterInfo.map((people) => {
+          return (
+          <Character 
+                    key={people.id}
+                    charName={people.name}
+                    home={people.homeworld}
+                    birth={people.birth_year}
+                />
+                )
+        })
+      }
     </div>
   )
 }
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function Character() { // ❗ Add the props
+function Character({charName, home}) { // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
+    const [show, setShow] = useState(false);
   // ❗ Create a "toggle" click handler to show or remove the homeworld
+    const toggleHomeWorld = () => {
+      setShow(!show)
+    }
+
   return (
-    <div>
+    <div className='character-card' onClick={toggleHomeWorld}>
       {/* Use the same markup with the same attributes as in the mock */}
+      <p className='character-name'>{charName}</p>
+      {show ? <p className='character-planet'>{home.name}</p> : null}
     </div>
   )
 }
